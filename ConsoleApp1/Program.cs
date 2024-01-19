9a072d2605f2e0c0fa626cc52751b95c5dd37f5b
haluk <haluk.ugur.celik@gmail.com>
2024-01-19 05:42:26 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace dortislem {
+    class toplama { 
+        static void Main(string[] args) { 
+Console.WriteLine("1 .sayi giriniz ");
+
+            int birinci = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2. sayi giriniz");
+            int ikinci = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ikinci+birinci);
+
+
+
+        }
+}
+}
